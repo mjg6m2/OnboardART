f2e23eb28263cf9ee21f77db592d1619885c152b
mjg6m2 <142945533+mjg6m2@users.noreply.github.com>
2024-11-11 17:27:12 -0600

    Database connection
    Added a connection to MongoDB Atlas

--- EMS/EMSServer/index.js
+++ EMS/EMSServer/index.js
@@ -1,11 +1,23 @@
 import express from 'express'
 import cors from 'cors'
+import mongoose from 'mongoose'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`)
+// Connect to MongoDB and start the server
+mongoose.connect(process.env.MONGODB_URI, {
+    //useNewUrlParser: true,
+    //useUnifiedTopology: true
 })
+    .then(() => {
+        console.log("Connected to MongoDB");
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    })
+    .catch(err => {
+        console.error("MongoDB connection error:", err);
+        process.exit(1); // Exit the app if the database connection fails
+    });
