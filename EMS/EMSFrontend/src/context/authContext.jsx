78640b5ac3776ccf2be66a1d26dd9f619d454b80
Phil Clever <ptcw77@umsystem.edu>
2024-11-12 18:08:32 -0600

    Created Login Ability
    Followed the steps in the Code with Yousaf video regarding login.

---
+++ EMS/EMSFrontend/src/context/authContext.jsx
@@ -0,0 +1,56 @@
+import React, { createContext, useState, useEffect, useContext } from 'react';
+
+
+const UserContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const verifyUser = async () => {
+            try {
+                const token = localStorage.getItem('token')
+                if(token) {
+                const response = await axios.get('http://localhost:3000/api/auth/verify', {
+                headers: {
+                    "Authorization" : 'Bearer ${token}'
+                }
+            })
+                if(response.data.success) {
+                    setUser(response.data.user)
+                }
+            } else {
+                setUser(null)
+            }
+            } catch(error) {
+                if(error.response && !error.response.data.error) {
+                    setUser(null)
+                }
+            } finally {
+                setLoading(false)
+            }
+        }
+        verifyUser()
+    }, []) 
+
+    const login = (user) => {
+        setUser(user);
+    };
+
+    const logout = () => {
+        setUser(null);
+        localStorage.removeItem("token");
+    };
+
+    return (
+        <UserContext.Provider value={{ user, login, logout, loading}}>
+            {children}
+        </UserContext.Provider>
+    );
+};
+
+// Custom hook to use the auth context
+export const useAuth = () => useContext(UserContext);
+
+export default AuthProvider;
