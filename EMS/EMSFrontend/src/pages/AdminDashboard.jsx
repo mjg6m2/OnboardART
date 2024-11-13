0bcc737fab123657b306d2ba7bfbd057bcf66e36
mjg6m2 <142945533+mjg6m2@users.noreply.github.com>
2024-11-13 16:25:04 -0600

    Remove code
    Removed code to make way for private routes

--- EMS/EMSFrontend/src/pages/AdminDashboard.jsx
+++ EMS/EMSFrontend/src/pages/AdminDashboard.jsx
@@ -1,17 +1,9 @@
 import React from 'react'
 import { useAuth } from '../context/authContext'
-import { useNavigate } from 'react-router-dom'
 
 const AdminDashboard = () => {
-  const { user, loading } = useAuth()
-  const navigate = useNavigate()
+  const {user} = useAuth()
 
-  if(loading) {
-    return <div>Loading....</div>
-  }
-  if(!user) {
-    navigate('/login')
-  }
   return (
     <div>AdminDashboard {user && user.name}</div>
   )
