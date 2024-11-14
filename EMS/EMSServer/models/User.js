bcbe981cf989f87f83c044715875825b9d831cb4
mjg6m2 <142945533+mjg6m2@users.noreply.github.com>
2024-11-13 23:35:21 -0600

    Update
    Changed word to match main

--- EMS/EMSServer/models/User.js
+++ EMS/EMSServer/models/User.js
@@ -7,7 +7,7 @@ password: {type: String, required: true},
 role: {type: String, enum: ["admin", "employee"], required: true},
 profilePic: {type: String},
 createAt: {type: Date, default Date.now},
-updatedAt: {type: Date, default Date.now},
+updateAt: {type: Date, default Date.now},
 })
 
 const User = mongoose.model("User", userSchema)
