ab7ee92fd7e612eb4754e73928cacbea8d6b6af9
mjg6m2 <142945533+mjg6m2@users.noreply.github.com>
2024-11-13 23:34:04 -0600

    Update
    Changed word to match branch

--- EMS/EMSServer/models/User.js
+++ EMS/EMSServer/models/User.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema({
     role: {type: String, enum: ["admin", "employee"], required: true},
     profileImage: {type: String},
     createAt: {type: Date, default: Date.now},
-    updatedAt: {type: Date, default: Date.now},
+    updateAt: {type: Date, default: Date.now},
 })
 
 const User = mongoose.model("User", userSchema)
