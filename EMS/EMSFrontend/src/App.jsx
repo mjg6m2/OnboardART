897f40ad7538c1d63219a75911401f066cd13960
mjg6m2 <142945533+mjg6m2@users.noreply.github.com>
2024-11-14 18:54:13 -0600

    Fixed dashboard UI
    Corrected the problem by changing 1 to l. Added min to avoid component interference. Wrapped AuthProvider around App

--- EMS/EMSFrontend/src/App.jsx
+++ EMS/EMSFrontend/src/App.jsx
@@ -1,4 +1,6 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './context/authContext'; // Import AuthProvider
 import Login from "./pages/Login";
 import AdminDashboard from './pages/AdminDashboard';
 import EmployeeDashboard from './pages/EmployeeDashboard';
@@ -7,32 +9,32 @@ import RoleBasedRoutes from './utils/RoleBasedRoutes';
 import AdminSummary from './components/dashboard/AdminSummary';
 import DepartmentList from './components/departments/DepartmentList.jsx';
 
-
-
 function App() {
-
   return (
-  <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Navigate to="/admin-dashboard" />}></Route>
-    <Route path="/login" element={<Login />}></Route>
-    <Route path="/admin-dashboard" element={
-      <PrivateRoutes>
-        <RoleBasedRoutes requiredRole = {["admin"]}>
-          <AdminDashboard />
-        </RoleBasedRoutes>
-      </PrivateRoutes>
-
-      }>
-        <Route index element = {<AdminSummary/>}></Route>
-
-        <Route path = "/admin-dashboard/department" element = {<DepartmentList/>}></Route>
-      
-      </Route>
-    <Route path="/employee-dashboard" element={<EmployeeDashboard />}></Route>
-  </Routes>
-  </BrowserRouter>
- );
+    <AuthProvider> {/* Wrap the entire app in AuthProvider */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/admin-dashboard" />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/admin-dashboard"
+            element={
+              <PrivateRoutes>
+                <RoleBasedRoutes requiredRole={["admin"]}>
+                  <AdminDashboard />
+                </RoleBasedRoutes>
+              </PrivateRoutes>
+            }
+          >
+            <Route index element={<AdminSummary />} />
+            <Route path="/admin-dashboard/department" element={<DepartmentList />} />
+          </Route>
+          <Route path="/employee-dashboard" element={<EmployeeDashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
 export default App;
+
--- EMS/EMSFrontend/src/pages/AdminDashboard.jsx
+++ EMS/EMSFrontend/src/pages/AdminDashboard.jsx
@@ -11,7 +11,7 @@ const AdminDashboard = () => {
   return (
     <div className = 'flex'>
       <AdminSidebar/>
-      <div className = 'flex-1 m1-64 bg-gray-100 h-screen'>
+      <div className = 'flex-1 ml-64 bg-gray-100 min-h-screen'>
         <Navbar/>
         <Outlet/>
       </div>
