1c7095bfd67a058f34ce0fdbdfcea07197c5c1c1
Phil Clever <ptcw77@umsystem.edu>
2024-11-19 11:41:58 -0600

    Employee Tab Functionality

--- EMS/EMSFrontend/src/App.jsx
+++ EMS/EMSFrontend/src/App.jsx
@@ -1,51 +1,83 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider } from './context/authContext'; // Import AuthProvider
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
-import AdminDashboard from './pages/AdminDashboard';
-import EmployeeDashboard from './pages/EmployeeDashboard';
-import PrivateRoutes from './utils/PrivateRoutes';
-import RoleBasedRoutes from './utils/RoleBasedRoutes';
-import AdminSummary from './components/dashboard/AdminSummary';
-import DepartmentList from './components/departments/DepartmentList.jsx';
-import AddDepartment from './components/departments/AddDepartment.jsx';
-import EditDepartment from './components/departments/EditDepartment.jsx';
+import AdminDashboard from "./pages/AdminDashboard";
+import EmployeeDashboard from "./pages/EmployeeDashboard";
+import PrivateRoutes from "./utils/PrivateRoutes";
+import RoleBaseRoutes from "./utils/RoleBasedRoutes"; 
+import AdminSummary from "./components/dashboard/AdminSummary";
+import DepartmentList from "./components/departments/DepartmentList";
+import AddDepartment from "./components/departments/AddDepartment";
+import EditDepartment from "./components/departments/EditDepartment";
+import List from "./components/employee/List";
+import Add from "./components/employee/Add";
+import View from "./components/employee/View";
+import Edit from "./components/employee/Edit";
+import Summary from './components/EmployeeDashboard/Summary'
+import Setting from "./components/EmployeeDashboard/Setting";
 
 function App() {
   return (
-   <AuthProvider>
     <BrowserRouter>
       <Routes>
-        {/* Redirect from root to admin dashboard */}
-        <Route path="/" element={<Navigate to="/admin-dashboard" />} />
-        
-        {/* Login route */}
-        <Route path="/login" element={<Login />} />
-        
-        {/* Admin dashboard with nested routes */}
-        <Route path="/admin-dashboard" element={
-          <PrivateRoutes>
-            <RoleBasedRoutes requiredRole={["admin"]}>
-              <AdminDashboard />
-            </RoleBasedRoutes>
-          </PrivateRoutes>
-        }>
-          {/* Admin dashboard summary */}
-          <Route index element={<AdminSummary />} />
-          
-          {/* Departments route as a nested route under admin-dashboard */}
-          <Route path="/admin-dashboard/departments" element={<DepartmentList />} />
-          <Route path="/admin-dashboard/add-department" element={<AddDepartment />} />
-          <Route path="/admin-dashboard/department/:id" element={<EditDepartment />} />
-        </Route>
+        <Route path="/" element={<Navigate to="/admin-dashboard" />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route
+          path="/admin-dashboard"
+          element={
+            <PrivateRoutes>
+              <RoleBaseRoutes requiredRole={["admin"]}>
+                <AdminDashboard />
+              </RoleBaseRoutes>
+            </PrivateRoutes>
+          }
+        >
+          <Route index element={<AdminSummary />}></Route>
 
-        {/* Employee dashboard */}
-        <Route path="/employee-dashboard" element={<EmployeeDashboard />} />
+          <Route
+            path="/admin-dashboard/departments"
+            element={<DepartmentList />}
+          ></Route>
+          <Route
+            path="/admin-dashboard/add-department"
+            element={<AddDepartment />}
+          ></Route>
+          <Route
+            path="/admin-dashboard/department/:id"
+            element={<EditDepartment />}
+          ></Route>
+
+          <Route path="/admin-dashboard/employees" element={<List />}></Route>
+          <Route path="/admin-dashboard/add-employee" element={<Add />}></Route>
+          <Route
+            path="/admin-dashboard/employees/:id"
+            element={<View />}
+          ></Route>
+          <Route
+            path="/admin-dashboard/employees/edit/:id"
+            element={<Edit />}
+          ></Route>
+
+          <Route path="/admin-dashboard/setting" element={<Setting />}></Route>
+        </Route>
+        <Route
+          path="/employee-dashboard"
+          element={
+            <PrivateRoutes>
+              <RoleBaseRoutes requiredRole={["admin", "employee"]}>
+                <EmployeeDashboard />
+              </RoleBaseRoutes>
+            </PrivateRoutes>
+          }
+        >
+          <Route index element={<Summary />}></Route>
+
+          <Route path="/employee-dashboard/profile/:id" element={<View />}></Route>
+          <Route path="/employee-dashboard/setting" element={<Setting />}></Route>
+
+        </Route>
       </Routes>
     </BrowserRouter>
-  </AuthProvider>
   );
 }
 
 export default App;
-
